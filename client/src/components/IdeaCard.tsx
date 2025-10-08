d0620168d50c4e579776cfbc92f1b470f8d52f4e
0GI4 <pop9991@outlook.com>
2025-10-09 01:23:10 +0300

    setup prisma orm and api proxy

--- client/src/components/IdeaCard.tsx
+++ client/src/components/IdeaCard.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Idea } from "../types";
 import "./ideas.css";
 import { ListItem, ListItemButton, ListItemText } from "@mui/material";
 
 const IdeaCard = ({ idea }: { idea: Idea }) => {
+  useEffect(() => {
+    fetch("/api/").then((res) => console.log(res));
+  }, []);
   return (
     <ListItem>
       <ListItemText style={{ width: "60%" }}>{idea.text}</ListItemText>
       <ListItemText
-        style={{ width: "30%", color: 'green' }}
+        style={{ width: "30%", color: "green" }}
       >{`Уже проголосвали ${idea.votes}`}</ListItemText>
-      <ListItemButton  style={{ width: "20%" }}>Проголосовать</ListItemButton>
+      <ListItemButton style={{ width: "20%" }}>Проголосовать</ListItemButton>
     </ListItem>
   );
 };
