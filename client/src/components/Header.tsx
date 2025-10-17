e011a24ad5a7ac5205e7000c90937a74ee561b96
0GI4 <pop9991@outlook.com>
2025-10-18 01:37:49 +0300

    header styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import SortDropdown, { SortKey } from "./Dropdown";
+import "./ideas.css";
 
 const Header = () => {
   const [sort, setSort] = useState<SortKey>("popular");
   return (
-    <header>
-      <h1>Идеи по развитию</h1>
-      <SortDropdown value={sort} onChange={setSort} />
+    <header className="ideas_header">
+      <div className="ideas_header-leftSide">
+        {" "}
+        <h1>Идеи по развитию</h1>
+      </div>
+      <div className="ideas_header-rightSide">
+        {" "}
+        <SortDropdown value={sort} onChange={setSort} />
+      </div>
     </header>
   );
 };
